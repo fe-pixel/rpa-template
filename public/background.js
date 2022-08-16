4127b4c92a56836744f912c09f4dd44e7c467dd2
aihuaqiao <aihuaqiao@joyy.com>
2022-08-16 18:33:43 +0800

    fix:修正插件跳转第二次跳转不到index.html的问题

--- public/background.js
+++ public/background.js
@@ -1,8 +1,6 @@
 
 
 //加载就跳转到index.html
-chrome.runtime.onInstalled.addListener(function (reason) {
-  chrome.tabs.update({ "url": `${window.location.origin}/index.html`, "selected": true });
-});
+chrome.tabs.update({ "url": `${window.location.origin}/index.html`, "selected": true });
 
 
